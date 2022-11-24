a884cb488346e4af78266f3107fe780cf116a5f0
PovarIsSFEDU <plukash@sfedu.ru>
2022-11-24 23:22:46 +0300

    Removed blank methods.

--- src/main/java/com/holydev/fastcase/controllers/AdminApiController.java
+++ src/main/java/com/holydev/fastcase/controllers/AdminApiController.java
@@ -1,38 +1,11 @@
 package com.holydev.fastcase.controllers;
 
 
-import com.holydev.fastcase.entities.Role;
-import com.holydev.fastcase.utilities.primitives.FilterObject;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.security.RolesAllowed;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminApiController {
 
-
-    @RolesAllowed({Role.BOSS})
-    @PostMapping("/podvig/all")
-    public void all_podvigs(@RequestBody FilterObject filter) {
-
-    }
-
-    @RolesAllowed({Role.BOSS})
-    @GetMapping("/podvig/{podvig_id}")
-    public void podvig(@PathVariable Long podvig_id) {
-
-    }
-
-    @RolesAllowed({Role.BOSS})
-    @GetMapping("/podvig/{podvig_id}/accept")
-    public void accept_podvig(@PathVariable Long podvig_id) {
-
-    }
-
-    @RolesAllowed({Role.BOSS})
-    @GetMapping("/podvig/{podvig_id}/decline")
-    public void decline_podvig(@PathVariable Long podvig_id) {
-
-    }
 }
